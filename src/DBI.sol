e5685b18a9b35102968de6f491c1452ae44a93cf
rwin <erwinphanglius10@gmail.com>
2023-12-09 02:53:07 +0700

    add setter & getter for orgs

--- src/DBI.sol
+++ src/DBI.sol
@@ -1,14 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-contract DBI {
-    uint256 public number;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+error IndexOutOfBound();
+
+contract DBI is Ownable {
+
+    struct Organisation {
+        string name;
+        address token;
+        address treasury;
+        uint256 thresholdTokenAmount;
+        string twitterName;
     }
 
-    function increment() public {
-        number++;
+    Organisation[] _organisations;
+
+    event OrganisationCreated(Organisation orgs); 
+
+    constructor() {}
+
+    function addOrganisation(Organisation memory orgs) external {
+        _organisations.push(orgs);
+
+        emit OrganisationCreated(orgs);
     }
-}
+
+    function getAllOrgs() external view returns (Organisation[] memory orgs) {
+        return _organisations;
+    }
+
+    function getOrgsDetailsById(uint256 index) external view returns (Organisation memory orgs) {
+        if(index > _organisations.length) {
+            revert IndexOutOfBound();            
+        }
+        return _organisations[index];
+    }
+    
+}   
